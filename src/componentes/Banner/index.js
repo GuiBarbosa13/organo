9b6716d9fb2621943ef27dfdf8f1b47ad60e7c4e
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-04-02 18:20:43 -0300

    Primeiro commit

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+        <header className = 'banner'>
+        <img src = "/banner.png" alt= "Banner principal da página da organo" className ='banner-img'/>
+        </header>
+    )
+}
+
+export default Banner;
---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,66 @@
+import './formulario.css';
+import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
+import { useState } from 'react';
+
+const Formulario = () => {
+
+    const times = ['Front-end', 'DevOps', 'Back-end', 'Dados'];
+
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState('');
+
+    const [time, setTime] = useState(times[0]);
+
+    
+
+    const enviarForm = (evento) => {
+        evento.preventDefault();
+        console.log('Formulário enviado! =>', nome, cargo, imagem, time)
+    };
+
+    return (
+        
+        <section className='formulario'>
+            <form onSubmit={enviarForm}>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <CampoTexto 
+                    obrigatorio ={true}
+                    label="Nome" 
+                    placeholder="Digite seu nome"
+                    valor = {nome}
+                    aoAlterado = {valor => setNome(valor)}
+                />
+
+                <CampoTexto 
+                    obrigatorio ={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo"
+                    valor = {cargo}
+                    aoAlterado = {valor => setCargo(valor)}
+                />
+
+                <CampoTexto 
+                    label="Imagem" 
+                    placeholder="Informe o endereço da imagem" 
+                    valor = {imagem}
+                    aoAlterado = {valor => setImagem(valor)}
+                />
+
+                <ListaSuspensa 
+                    label ="Time" 
+                    itens ={times}
+                    valor = {time}
+                    aoAlterado = {valor => setTime(valor)}
+                />
+
+                <Botao>Criar card</Botao> {/*É POSSÍVEL PASSAR PARA A TELA FINAL ITENS ANINHADOS USANDO O PROPS.CHILDREN*/}
+            </form>
+
+        </section>
+    )
+}
+
+export default Formulario
