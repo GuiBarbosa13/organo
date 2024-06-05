54da97d75bc808364ed3605eb52fde9fcc87dfe8
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-06-05 15:39:19 -0300

    migrando os imputs para TS

---
+++ src/componentes/Campo/index.tsx
@@ -0,0 +1,27 @@
+import './Campo.css';
+
+interface CampoProps{
+    aoAlterado: (valor: string) => void,
+    obrigatorio: boolean,
+    label: string,
+    placeholder: string,
+    type: string,
+    valor: string,
+}
+
+const Campo = ({aoAlterado, obrigatorio, label, placeholder, type, valor}:CampoProps) =>{
+
+    const aoDigitar = (evento:React.ChangeEvent<HTMLInputElement>) =>{
+        aoAlterado(evento.target.value);
+    };
+
+    return(
+        <div className={`campo campo-${type}`}>
+            <label>{label}</label>
+            <input value = {valor} onChange={aoDigitar} required = {obrigatorio}  placeholder = {placeholder}
+            type={type}/>
+        </div>
+    )
+}
+
+export default Campo;
